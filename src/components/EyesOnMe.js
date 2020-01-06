f70abfa9469a3c7029f0b81463c626911b068afa
mattbechtel1 <bechma02@gmail.com>
2020-01-06 16:51:08 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+    let focusCallback = () => (console.log('Good!'))
+    let blurCallback = () => (console.log('Hey! Eyes on me!'))
+    
+    let myBtn = <button onFocus={focusCallback} onBlur={blurCallback} />
+    
+    return myBtn
+}
+
+export default EyesOnMe
